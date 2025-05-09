9252275f88b8ba477f54756f7e886488b3d14942
3rr0r445 <TravisEDuran@gmail.com>
2025-05-09 00:42:48 -0400

    Add additional explanations and examples in main.py to enhance understanding of print function and variable usage

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import random
 
+
+#A basic introduction about what I'm doing, mostly rambling.
 def main():
     print("I don't really know what I'm doing here.")
     print("I think I'm just practicing the functions I know and also trying to practice github commands.")
     print("Let's see how it goes, shall we?")
+
+#Explain what the print function does and use it in a couple examples.
+    print("Calling the print function allows you to output text directly to the screen")
+    print("It's the simplest and one of the first functions you learn in Python.")
+    print("Hello, World and all that.")
+
+    #Define a couple variables to use in further examples
+    my_name = "Travis"
+
+    print("You can use variables to hold values to use.")
+    print(my_name)
+    
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
