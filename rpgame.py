e2e5270cb1c6b55c62fcd42eec0ee708088b90bb
3rr0r445 <TravisEDuran@gmail.com>
2025-05-09 01:13:12 -0400

    Add rpgame.py to introduce a narrative-driven game setup with initial print statements for player immersion

---
+++ rpgame.py
@@ -0,0 +1,7 @@
+import random
+
+print("The outbreak has begun. The news fell silent hours ago.")
+print("You smell smoke and the sounds of gunfire and screams echo through your apartment.")
+print("It's time to go. You simply cannot stay here any longer.")
+print()
+print()
