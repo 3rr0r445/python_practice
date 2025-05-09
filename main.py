f6b810e1c73d19aa274908482e48ae40a5dd6f0e
3rr0r445 <TravisEDuran@gmail.com>
2025-05-09 00:35:56 -0400

    Add main.py with initial function practice and print statements

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+
+def main():
+    print("I don't really know what I'm doing here.")
+    print("I think I'm just practicing the functions I know and also trying to practice github commands.")
+    print("Let's see how it goes, shall we?")
