2890b677ecab94e449f4304901913e2689cf30d9
3rr0r445 <TravisEDuran@gmail.com>
2025-05-09 00:46:57 -0400

    Enhance main.py with additional print statements for clarity and flow in examples

--- main.py
+++ main.py
@@ -6,18 +6,32 @@ def main():
     print("I don't really know what I'm doing here.")
     print("I think I'm just practicing the functions I know and also trying to practice github commands.")
     print("Let's see how it goes, shall we?")
+    print()
 
 #Explain what the print function does and use it in a couple examples.
     print("Calling the print function allows you to output text directly to the screen")
     print("It's the simplest and one of the first functions you learn in Python.")
     print("Hello, World and all that.")
-
+    print()
     #Define a couple variables to use in further examples
     my_name = "Travis"
 
     print("You can use variables to hold values to use.")
+    print()
     print(my_name)
+    print()
+    print("Now, you can't see it, but I just called a variable called my_name")
+    print("I defined the value 'my_name = 'Travis'")
+    print("Then I called it with the print command like print('my_name')")
+    print()
+    print("This is actually kind of difficult to type like this.")
+    print("It makes more sense to look at the code then the actual output.")
+    print("Insanity.")
+    print()
+    print()
+    print("Anyway, let's move on.")
     
+
 
 
 
