7b3b356e3599d499b9611c30a3a054f18d9cd6d3
3rr0r445 <TravisEDuran@gmail.com>
2025-05-09 00:37:38 -0400

    Refactor main.py to include a main guard for script execution

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ def main():
     print("I don't really know what I'm doing here.")
     print("I think I'm just practicing the functions I know and also trying to practice github commands.")
     print("Let's see how it goes, shall we?")
+
+if __name__ == "__main__":
+    main()
