7e564c0027435a71643a5bec5033293660ab4b97
3rr0r445 <TravisEDuran@gmail.com>
2025-06-01 04:04:07 -0400

    Refactor rpgame.py to improve game introduction and living room scenario, adding narrative depth and player choices

--- rpgame.py
+++ rpgame.py
@@ -1,37 +1,73 @@
 import random
+import time
 
-#game setup
-health = 100
-inventory = []
-
-
-#introduction to the game
-print("The outbreak has begun. The news fell silent hours ago.")
-print("You smell smoke and the sounds of gunfire and screams echo through your apartment.")
-print("It's time to go. You simply cannot stay here any longer.")
-print()
-print("You have to move from the safety of your home.")
-print("There are only a few places you can go in town and the choice is up to you.")
+def main():
+    game_intro()
+    living_room()
 
 
 
-print("\n\n")
-print("A sudden pounding from your front door shakes you from your panic.")
-print("You're entirely out of time. You stand from your couch and look around")
-print("What do you do?")
-print("     1) Answer your door")
-print("     2) Look for supplies")
-print("     3) Run to your backdoor")
+def game_intro():
+    print("Welcome to Travis' Zombie Survival Game!")
+    print("Created by: Travis Duran")
+    print("Special Thanks to: CodeinPlace and Stanford.")
+    print("Also a huge thanks to my fellow students in my section!")
+    print()
+    input("Press Enter to Begin...")
+    print()
+    print()
+    print("--------------------------------")
+    print("The outbreak has begun. The news fell silent hours ago.")
+    time.sleep(1)
+    print("You smell smoke and something else you can't quite place, something sweet and sickly.")
+    time.sleep(1)
+    print("Gunfire, screams—once constant. Now gone. The world outside is eerily still.")
+    time.sleep(1)
+    print("What you can vaguely hear from outside is the sound of shambling footsteps and moaning.")
+    time.sleep(1)
+    print("Escape is probably a pipedream at this point. Maybe something in this house can help you survive.")
+    time.sleep(1)
+    print()
 
-choice = input("> ").strip()
-
-if choice == 1:
-    print("You cautiously begin to open the door.")
-    print("A rotten claw-like hand reaches through the crack and scratches you!")
-    print("You slam the door closed and lock it, but the scratch on your arm hurts.")
-    global health
-    health = 25
-    print(health)
 
 
 
+def living_room():
+    living_room_intro()
+    living_room_choice()
+
+def living_room_intro():
+    print("The living room isn't much to look at, but it's your home.")
+    time.sleep(1)
+    print("A city map lies on the table, but you know it's not safe to go outside anymore.")
+    time.sleep(1)
+    print("As you sit on the couch plotting your next move..")
+    time.sleep(1)
+    print("A series of loud BANG rattles the front door on it's hinges.")
+    time.sleep(1)
+    print("You instinctively hold your breath, your heart pounding in your chest.")
+    time.sleep(2)
+    print("BANG! BANG! BANG!")
+    time.sleep(1)
+    print("Your next decision is critical. Your choices are few.")
+    time.sleep(1)
+    print()
+    print()
+    
+
+def living_room_choice():
+    print("What should you do? ")
+    print("1) Open the door (Why would you do this?")
+    print("2) Lock the door and try to remain as hidden as possible.")
+    print("3) Leave the living room and try to find a something useful.")
+    print("4) Search the Living Room for supplies.")
+    print()
+    living_room_choice = input("Enter your choice: ")
+
+    if living_room_choice == "1":
+        print("You confidently walk to the door and throw it open.")
+
+    
+
+if __name__ == '__main__':
+    main()
